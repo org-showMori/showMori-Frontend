a185b2b29d3d5e68b2ea82e8f350c3c51c803dda
SOHYEON00 <sohyeon0amy@gmail.com>
2020-08-25 17:21:04 +0900

    til #2.2 practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const food = [
+  {
+    name: "한현희",
+    place: "카페"
+  },
+  {
+    name: "오소현",
+    place: "카페"
+  },
+  {
+    name: "서혜진",
+    place: "병원"
+  },
+  {
+    name: "차유빈",
+    place: "학교"
+  },
+  {
+    name: "이성경",
+    place: "??"
+  }
+];
+function People({name, place}) {
+  return (
+    <div>
+      <h3>{name}은 지금 {place}에 있어요.</h3>
+    </div>
+  );
+}
+function printEveryone(people) {
+  return <People name={people.name} place={people.place} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {food.map(printEveryone)}
     </div>
   );
 }

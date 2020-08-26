87e975f62d853d4f9bb014e5993246736b332f7e
SOHYEON00 <sohyeon0amy@gmail.com>
2020-08-26 23:57:11 +0900

    about STATE & React LifeCycle API -#3.2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import React from 'react';
 
-const food = [
-  {
-    name: "한현희",
-    place: "카페"
-  },
-  {
-    name: "오소현",
-    place: "카페"
-  },
-  {
-    name: "서혜진",
-    place: "병원"
-  },
-  {
-    name: "차유빈",
-    place: "학교"
-  },
-  {
-    name: "이성경",
-    place: "??"
-  }
-];
-function People({name, place}) {
-  return (
-    <div>
-      <h3>{name}은 지금 {place}에 있어요.</h3>
-    </div>
-  );
-}
-function printEveryone(people) {
-  return <People name={people.name} place={people.place} />;
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-function App() {
-  return (
-    <div>
-     {food.map(printEveryone)}
-    </div>
-  );
+  add = () => {
+    this.setState(hey => ({count: hey.count + 1}));
+  }
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  }
+  // Component Life Cycle APIs
+  // 컴포넌트가 DOM에 생성되기 전, 후나 업데이트되기 전, 후로 실행되는 메소드들
+  //*** constructor(props) : 개인적으로 찾아보기 *** 
+
+  // 1. when component is insulted into the DOM
+  componentDidMount() {
+    console.log("I created");
+  }
+
+  // 2. 실제 DOM이 업데이트하기 전에 업데이트 해야하는지 알아보기 위해 가상 DOM에 컴포넌트를 재 render하는 단계
+  componentDidUpdate() {
+    console.log("I'm updated");
+  }
+  // 3. 컴포넌트가 DOM에서 해제되는 단계
+  componentWillUnmount() {
+    console.log("I'm died");
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The count is : {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;

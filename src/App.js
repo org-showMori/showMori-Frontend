5f938a477ae409b96d83450f5757f3761589ece3
SOHYEON00 <sohyeon0amy@gmail.com>
2020-08-31 00:29:16 +0900

    벨로퍼트 리액트 공부 실습

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
+import Counter from './Counter';
 
 class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  componentDidMount() {
-    setTimeout( () => {
-      this.setState({ isLoading: false });
-      // ***** state를 직접적으로 바로 건들이는 것은 매우 위험한 습관. setState를 습관하하자.
-    }, 6000);
-  }
-
   render() {
-    const { isLoading } = this.state; // Q. 정확한 이해 필요
-    return <div>{isLoading ? "Loading..." : "Complete!"}</div>;
+    return (
+      <Counter  />
+    );
   }
 }
 
---
+++ src/MyName.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyName = ({ name}) => {
+    return <div>hello! Im {name}</div>;
+};
+
+export default MyName;

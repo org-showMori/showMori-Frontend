67af7fbfaed60844d4345e8541070fd7fa77b3e0
SOHYEON00 <sohyeon0amy@gmail.com>
2020-09-02 00:19:01 +0900

    벨로퍼트 리액트 전화번호부 실습

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import PhoneForm from './componenets/PhoneForm';
+import PhoneInfoList from './componenets/PhoneInfoList';
 
 class App extends React.Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '오소현',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-1111-1111'
+      }
+    ]
+  }
+
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data }),
+    });
   }
   render() {
+    // const {information} = this.state;
     return (
       <div>
-        <PhoneForm 
-        onCreate={this.handleCreate}/>
-      </div>
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList data={this.state.information} />
 
+      </div>
     );
   }
 }
--- src/MyName.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const MyName = ({ name}) => {
-    return <div>hello! Im {name}</div>;
-};
-
-export default MyName;
